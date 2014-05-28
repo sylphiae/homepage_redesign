ad5d132e31cbb01eb469b368fcd5c06423514f79
Dan Lopuch <dan@datahero.com>
2014-05-27 19:35:26 -0700

    Hooking up auth server request, initial login layout stub

--- js/login_modal.js
+++ js/login_modal.js
@@ -1,3 +1,17 @@
 $(function() {
-  console.log("login modal controller success");
+  $.get('https://auth.mayone.us/v1/current_user?return_to=https://mayone.us/homepage_redesign')  // TODO: template-ify the returnTo
+  .fail(function() {
+    $('#mysuperpac_topmenu').hide();
+    $('#login_topmenu').hide();
+    throw new Error('Error reaching auth server');
+  })
+  .done(function(authCreds) {
+    if (authCreds.logged_in) {
+      $('#mysuperpac_topmenu').show();
+      $('#login_topmenu').hide();
+      return;
+    }
+
+
+  });
 });
