094cd6c29dec9f0d8fcfb5a98b24ff51585a101b
Dan Lopuch <dan@datahero.com>
2014-05-27 19:39:41 -0700

    Showing auth buttons

--- js/login_modal.js
+++ js/login_modal.js
@@ -12,6 +12,10 @@ $(function() {
       return;
     }
 
+    // else, we need to login.  Show the buttons to toggle the modal
+    $('#login_topmenu').show();
+    $('#mysuperpac_topmenu').hide();
+
 
   });
 });
