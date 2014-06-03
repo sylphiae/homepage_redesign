5b3453e7ee6f860676177026e60d030f015ade71
Kyle Stoneman <github@kylestoneman.com>
2014-06-03 17:10:22 -0400

    Fixed interior counter numbers

--- js/progress.js
+++ js/progress.js
@@ -27,7 +27,7 @@ var days_left_message = '18 Days Early';
 function totalRaisedCB(totalRaisedCents) {
   var totalRaised = totalRaisedCents / 100;
   // todo: this needs to change when we make it past 2million
-  var percent = Math.floor(totalRaised / 20000);
+  var percent = Math.floor((totalRaised - 1000000) / 10000);
   setText("super-cool-progress-bar-percent", '' + percent + '% Funded');
   setText("super-cool-progress-bar-funded", '$' + addCommas(totalRaised));
   document.getElementById("super-cool-progress-bar-bar").style.width='' + Math.min(100, percent) + '%';
