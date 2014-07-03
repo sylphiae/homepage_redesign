6b9627b308285aeda38cbfc279f6486d5d85f905
aaronlifshin <aaronlifshin@gmail.com>
2014-07-02 17:18:07 -0700

    still debugging

--- js/pledge.js
+++ js/pledge.js
@@ -282,7 +282,7 @@ $(document).ready(function() {
   
   $('#bitcoinButton').on('click', bitcoinPledge);
   
-  $.get(PLEDGE_URL + '/r/payment_config').done(function(pConf) {
+  $.get(PLEDGE_URL + '/r/payment_config', {}, {}, "json").done(function(pConf) {
     
       paymentConfig = pConf;
       console.log(pConf);
